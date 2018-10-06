1e7aa76bd2b46c3ce81477d2ab432daf8bde25ed
Genevieve <genschultz@gmail.com>
2018-10-06 14:43:52 -0700

    redux added to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import Hello from './containers/Hello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+// import { StoreState } from './types/index';
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
